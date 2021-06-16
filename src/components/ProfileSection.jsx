e1b3311727af1ef2a81df0aada5cc177fc1e2cac
clecardona <clecardona@gmail.com>
2021-06-16 09:57:35 +0200

    resp

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -1,4 +1,6 @@
-import { faGlassMartiniAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloudDownloadAlt } from "@fortawesome/free-solid-svg-icons";
+import { faJava } from "@fortawesome/free-brands-svg-icons";
 import portrait from "../assets/img/portrait.jpeg";
 
 export default function ProfileSection() {
@@ -9,6 +11,15 @@ export default function ProfileSection() {
         <p className="lastname">Cardona</p>
         <p className="position">Junior Full-stack developer</p>
         <p className="title">Portfolio</p>
+        <div className="cv">
+          Download my CV
+          <a href="https://clecardona.com/doc/CV_Clement_Cardona.pdf" target="blank">
+          <FontAwesomeIcon
+            icon={faCloudDownloadAlt}
+            className="icon"
+            size="2x"
+          /></a>
+        </div>
       </div>
       <div className="profile-right">
         <div className="container">
@@ -20,12 +31,11 @@ export default function ProfileSection() {
                 d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"
               />
               <text width="300">
-                <textPath
-                  startOffset="100%"
-                   textAnchor="end"
-                   href="#curve"
-                >
-                  <a href="mailto: clecardona@gmail.com" target="blank">clecardona@gmail.com</a> . 076-590-16-39
+                <textPath startOffset="100%" textAnchor="end" href="#curve">
+                  <a href="mailto: clecardona@gmail.com" target="blank">
+                    clecardona@gmail.com
+                  </a>{" "}
+                  . 076-590-16-39
                 </textPath>
               </text>
             </svg>
